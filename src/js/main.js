608bc842f75656a8ca76c030be96be43a4e9cb61
Jakub Wójcik <kubaton1994@o2.pl>
2020-03-31 17:33:56 +0200

    js to projects added

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,34 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
 console.log(`Hello world!`);
 
+const list = document.querySelector('.projects-list--js');
+if (list) {
+    fetch('https://api.github.com/users/kozirro/repos?sort=updated&direction=desc')
+        .then(resp => resp.json())
+        .then(resp => {
+            const repos = resp;
+            for (const repo of repos) {
+                list.innerHTML +=
+                    `<li class="project">
+                        <div class="project__container">
+                            <img class="project__logo" src="assets/img/GitHub-icon.svg" alt/>
+                            <h3 class="project__title">${repo.name}</h3>
+                            <p class="project_description"> ${repo.description ? repo.description : "brak opisu"} </p>
+                        </div>
+                        <div class="project__footer">
+                          ${
+                            repo.homepage ? '<a class="project__link project__link--demo" href="${repo.homepage}" title="demo: ${repo.name}">Demo</a>': ''
+                          }
+                          <a class="project__link project__link--code" href="${repo.html_url}" target="_blank" rel="nofollow noreferrer" title="github: ${repo.name}">GitHub</a>
+                        </div>
+                    </li>`;
+            }
+        })
+        .catch(err => {
+            console.log('errors');
+        })
 
+
+
+}
